3b7680924918e3c6d9dff8d7a904f9f6b5a5ffc1
saralotfi <saralotfi3442@gmail.com>
2024-11-10 21:46:04 +0330

    Refactored code to use a class structure for SnakeGame

--- snake.py
+++ snake.py
@@ -1,73 +1,77 @@
 import random
 import os
 import time
-width, height = 5, 4
-snake = [(2, 2)]
-direction = "RIGHT"
-food = (random.randint(1, height - 1), random.randint(1, width - 1))
-score = 0
-def draw():
-    os.system("cls" if os.name == "nt" else "clear") 
-    for y in range(height):
-        row = " "
-        for x in range(width):
-            if (y, x) == snake[0]:
-                row += " O "  
-            elif (y, x) in snake:
-                row += " () " 
-            elif (y, x) == food:
-                row += " * "  
-            else:
-                row += " - "  
-        print(row)
-    print(f"Score: {score}")
 
-def update():
-    global food, score 
-    head_y, head_x = snake[0]
-    if direction == "UP":
-        new_head = (head_y - 1, head_x)
-    elif direction == "DOWN":
-        new_head = (head_y + 1, head_x)
-    elif direction == "LEFT":
-        new_head = (head_y, head_x - 1)
-    elif direction == "RIGHT":
-        new_head = (head_y, head_x + 1)
+class SnakeGame:
+    def __init__(self):
+        self.width = 5
+        self.height = 4
+        self.snake = [(2, 2)]
+        self.direction = "RIGHT"
+        self.food = (random.randint(1, self.height - 1), random.randint(1, self.width - 1))
+        self.score = 0
 
-    if new_head[0] < 0 or new_head[0] >= height or new_head[1] < 0 or new_head[1] >= width:
-        return False
-    
-    if new_head in snake:
-        return False
+    def draw(self):
+        os.system("cls" if os.name == "nt" else "clear")
+        for y in range(self.height):
+            for x in range(self.width):
+                if (y, x) == self.snake[0]:
+                    print(" O ", end="")
+                elif (y, x) in self.snake:
+                    print(" () ", end="")
+                elif (y, x) == self.food:
+                    print(" * ", end="")
+                else:
+                    print(" - ", end="")
+            print()
+        print("Score:", self.score)
 
-    snake.insert(0, new_head)
+    def move(self):
+        head_y, head_x = self.snake[0]
+        if self.direction == "UP":
+            new_head = (head_y - 1, head_x)
+        elif self.direction == "DOWN":
+            new_head = (head_y + 1, head_x)
+        elif self.direction == "LEFT":
+            new_head = (head_y, head_x - 1)
+        elif self.direction == "RIGHT":
+            new_head = (head_y, head_x + 1)
+        return new_head
 
-    if new_head == food:
-        score += 1
-        food = (random.randint(1, height - 1), random.randint(1, width - 1))  
-    else:
-        snake.pop()  
+    def update(self):
+        new_head = self.move()
+        if (new_head[0] < 0 or new_head[0] >= self.height or
+            new_head[1] < 0 or new_head[1] >= self.width or
+            new_head in self.snake):
+            return False
 
-    return True
+        self.snake.insert(0, new_head)
 
+        if new_head == self.food:
+            self.score += 1
+            self.food = (random.randint(1, self.height - 1), random.randint(1, self.width - 1))
+        else:
+            self.snake.pop()
 
-try:
-    while True:
-        draw()
-        command = input("Move (U/D/L/R): ").upper()
-        if command == "U" and direction != "DOWN":
-            direction = "UP"
-        elif command == "D" and direction != "UP":
-            direction = "DOWN"
-        elif command == "L" and direction != "RIGHT":
-            direction = "LEFT"
-        elif command == "R" and direction != "LEFT":
-            direction = "RIGHT"
+        return True
 
-        if not update():
-            print("Game Over!")
-            break
+    def play(self):
+        while True:
+            self.draw()
+            command = input("Move (U/D/L/R): ").upper()
+            if command == "U" and self.direction != "DOWN":
+                self.direction = "UP"
+            elif command == "D" and self.direction != "UP":
+                self.direction = "DOWN"
+            elif command == "L" and self.direction != "RIGHT":
+                self.direction = "LEFT"
+            elif command == "R" and self.direction != "LEFT":
+                self.direction = "RIGHT"
 
-        time.sleep(0.2) 
-except KeyboardInterrupt:
-    print("\nGame interrupted!")
+            if not self.update():
+                print("Game Over!")
+                break
+            time.sleep(0.2)
+
+game = SnakeGame()
+game.play()
